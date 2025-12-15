4cbeecc1baedd0781b86fe8bc438b7ddca825b0c
dadaxer <dadaxer@mailbox.org>
2025-12-16 00:59:18 +0100

    Delete enable-sddm.sh

--- files/scripts/enable-sddm.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -oue pipefail
-
-# Remove the autologin to gamescope-session
-rm /etc/sddm.conf.d/steamos.conf
-echo "steamos.con removed"
-
-# Undo some bazzite-deck specific services we do not need anymore
-systemctl disable bazzite-autologin.service
